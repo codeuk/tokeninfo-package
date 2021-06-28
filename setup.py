9a7f5e351f93d4fd095ae7a3f65752ce9e6e8201
doop <75194878+7uk@users.noreply.github.com>
2021-06-28 16:37:50 +0100

    Pypi Setup

--- setup.py
+++ setup.py
@@ -1,31 +1,26 @@
-from setuptools import setuptools
-
-def readme():
-    with open('README.md') as f:
-        README = f.read()
-    return README
-	
+  
+from setuptools import setup, find_packages
+ 
+classifiers = [
+  'Development Status :: 5 - Production/Stable',
+  'Intended Audience :: Education',
+  'Operating System :: Microsoft :: Windows :: Windows 10',
+  'License :: OSI Approved :: MIT License',
+  'Programming Language :: Python :: 3'
+]
+ 
 setup(
-    name="discordinfo",
-    version="1.0.0",
-    description="discordinfo is a Python Package created for simply scraping discord account data with the use of an account token.",
-    long_description=readme(),
-    long_description_content_type="text/markdown",
-    url="https://github.com/7uk/discordinfo-package"
-    author="7uk",
-    author_email="doopycheats@gmail.com",
-    license="MIT",
-    classifiers=[
-        "License :: OSI Approved :: MIT License",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.7",
-    ],
-    packages=["discordinfo"],
-    include_package_data=False,
-    install_requires=["requests", "json"],
-    entry_points={
-        "console_scripts": [
-            "discordinfo=discord-info.discordlaunch:main",
-        ]
-    },
+  name='tokeninfo',
+  version='0.0.1',
+  description='tokeninfo is a Python Package created for simply scraping discord account data with the use of an account token.',
+  long_description=open('README.md').read(),
+  url='https://github.com/7uk/tokeninfo-package',  
+  author='7uk',
+  author_email='doopycheats@gmail.com',
+  license='MIT', 
+  classifiers=classifiers,
+  keywords='discord', 
+  packages=find_packages(),
+  install_requires=['requests', 'json'] 
 )
+	
